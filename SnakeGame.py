094743755e59685c2f1008b0e252f9c69be03143
daiz93 <desmond.kpo@gmail.com>
2025-01-25 07:48:05 +0100

    Fin initialisation de la fentre de jeux

--- SnakeGame.py
+++ SnakeGame.py
@@ -3,8 +3,30 @@ import sys
 
 #Initialiser pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Jeu du serpent")
-screen.blit(pygame.image.load("welcome.png"), [400, 300])
+
+# Paramètres de la fenêtre
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Jeux du serpent")
+
+# Couleurs
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
+
+screen.blit(pygame.image.load("welcome.png"), [0, 0])
 pygame.display.flip()
 pygame.time.wait(3000)
+
+
+running = True
+while running:
+    screen.fill(BLACK)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
