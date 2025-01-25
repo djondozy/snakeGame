401c4a9cb4759aeb62fd1d74cec0015bdae0678a
daiz93 <desmond.kpo@gmail.com>
2025-01-25 07:38:27 +0100

    Realisation de la fenettre de jeux

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,4 +1,10 @@
 import pygame
+import sys
 
 #Initialiser pygame
 pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Jeu du serpent")
+screen.blit(pygame.image.load("welcome.png"), [400, 300])
+pygame.display.flip()
+pygame.time.wait(3000)
